238de9670689f1de0c449bd60bdb40a0f0e05a36
Owen Carlsen <o@ocarlsen.com>
2022-11-13 15:35:03 -0800

    Adding MyInstanceLoggingClass

---
+++ log4j/junit40_43/src/test/java/com/ocarlsen/test/example/log4j/junit4/MyInstanceLoggingClassTestBase.java
@@ -0,0 +1,24 @@
+package com.ocarlsen.test.example.log4j.junit4;
+
+import com.ocarlsen.test.example.log4j.MyInstanceLoggingClass;
+
+public abstract class MyInstanceLoggingClassTestBase extends MyLoggingClassTestBase<MyInstanceLoggingClass> {
+
+    private static final String LOGGING_CLASS_NAME = "com.ocarlsen.test.example.log4j.MyInstanceLoggingClass";
+    public static final String LOGGER_FIELD_NAME = "logger";
+
+    @Override
+    protected String getLoggingClassName() {
+        return LOGGING_CLASS_NAME;
+    }
+
+    @Override
+    protected String getLoggerFieldName() {
+        return LOGGER_FIELD_NAME;
+    }
+
+    @Override
+    protected MyInstanceLoggingClass newLoggingInstance() {
+        return new MyInstanceLoggingClass();
+    }
+}
